56f8aa14ee93fecf83cb7e7fb14115072c39121a
mawilson1234 <michael.a.wilson@yale.edu>
2022-10-31 13:08:37 -0400

    update training session

--- data_includes/main.js
+++ data_includes/main.js
@@ -8,7 +8,7 @@ var counterOverride = 0
 var required_to_pass = 0.9
 var required_to_pass_perc = String(Math.round(required_to_pass * 100))
 var max_attempts = 5
-var MS_PER_WORD = 0 //325
+var MS_PER_WORD = 1 //325
 
 var blank_style = {
 	border: '1px solid #000', 
@@ -135,7 +135,6 @@ var feedback_trial = label => item => {
 	var postsentence = postsentence.startsWith(',') | postsentence.startsWith('.') ? postsentence : '&nbsp;' + postsentence
 	var first_arg	 = item.sentence.match(/\[(su|o)bj\]/g)[0]
 	var second_arg   = item.sentence.match(/\[(su|o)bj\]/g)[1]
-	var MS_PER_WORD  = MS_PER_WORD
 	
 	return newTrial(label,
 		newText("word", word).css({width: '', border: '1px solid #000', padding: '3px'}).center(),
