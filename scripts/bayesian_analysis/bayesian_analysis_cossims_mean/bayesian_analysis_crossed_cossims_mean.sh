c1410b0d1fcb198cd84bcf87521d4bf0ded11d1f
mawilson1234 <michael.a.wilson@yale.edu>
2022-12-01 15:00:40 -0500

    update script requirements

--- scripts/bayesian_analysis/bayesian_analysis_cossims_mean/bayesian_analysis_crossed_cossims_mean.sh
+++ scripts/bayesian_analysis/bayesian_analysis_cossims_mean/bayesian_analysis_crossed_cossims_mean.sh
@@ -3,11 +3,11 @@
 #SBATCH --job-name=salts_exp_bayes_crossed-cossims_mean
 #SBATCH --output=joblogs/%x_%j.txt
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=16G
-#SBATCH --partition=week
-#SBATCH --time=02-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=16G
+#SBATCH --partition=day
+#SBATCH --time=01-00:00:00
 #SBATCH --mail-type=FAIL,END,INVALID_DEPEND
 
 module load R/4.1.0-foss-2020b
